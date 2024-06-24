9314634dba524cf3316598ae5244d3038c9154aa
MohammadAnas070 <anasansari02092001@gmail.com>
2024-06-24 18:48:53 +0530

    Update settings.py

--- watercooler/settings.py
+++ watercooler/settings.py
@@ -125,10 +125,8 @@ USE_TZ = True
 STATIC_URL = 'static/'
 # Add these new lines
 
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build','static')
 
 
 MEDIA_URL = '/media/'

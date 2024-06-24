aa865c2f48280e0ba2a59c62c554e058d17eee10
MohammadAnas070 <anasansari02092001@gmail.com>
2024-06-24 18:45:09 +0530

    Update wsgi.py

--- watercooler/wsgi.py
+++ watercooler/wsgi.py
@@ -10,6 +10,6 @@ https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
 import os
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'your_project_name.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'watercooler.settings')
 application = get_wsgi_application()
 app = application

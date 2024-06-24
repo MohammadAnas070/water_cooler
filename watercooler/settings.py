9e198369e6b2f7700862914a49f57d82b5573412
MohammadAnas070 <anasansari02092001@gmail.com>
2024-06-24 17:40:52 +0530

    Update settings.py
    updated

--- watercooler/settings.py
+++ watercooler/settings.py
@@ -80,14 +80,10 @@ WSGI_APPLICATION = 'watercooler.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-'default': {
-'ENGINE': 'django.db.backends.mysql',
-'NAME': 'watercooler',
-'USER': 'root',
-'PASSWORD': '',
-'HOST':'localhost',
-'PORT':'3306',
-}
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
 
 
